2f00e92647655810275ad800d2888c64d4f5c77d
Red Williams <redpwilliams@gmail.com>
2023-11-28 15:55:57 -0500

    refactor(commitlint): clarify restrictions on head, body commits
    Now it shouldn't complain about the body as much. Also restored it to the 72 characters for a commit header

--- options/commits.rules.ts
+++ options/commits.rules.ts
@@ -2,9 +2,13 @@ import type { RulesConfig } from '@commitlint/types'
 import { CommitFields } from './commits.fields'
 
 export const CommitRules: Partial<RulesConfig> = {
-  'header-max-length': [2, 'always', 50],
+  'body-full-stop': [0, 'always', ''],
+  'body-leading-blank': [0, 'always'],
+  'body-empty': [0, 'always'],
+  'body-case': [0, 'always', 'sentencecase'],
+  'body-max-line-length': [0, 'always', Infinity],
   'type-enum': [2, 'always', Object.keys(CommitFields)],
   'type-case': [1, 'always', 'lower-case'],
-  'scope-case': [2, 'always', 'kebab-case' || 'lowerCase'],
+  'scope-case': [2, 'always', 'lowerCase'],
   'scope-empty': [2, 'never']
 } as const
