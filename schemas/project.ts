122ab3c7f9c30e9fe088e02bb3951c18bb9325af
Red Williams <redpwilliams@gmail.com>
2023-11-29 03:48:42 -0500

    feat(project.ts): add type safety to project schema

--- schemas/project.ts
+++ schemas/project.ts
@@ -1,22 +1,71 @@
-export default {
+import { defineType, defineField } from 'sanity'
+
+/**
+ * Things to consider adding:
+ * - Description (might go better with the blog part, maybe not),
+ * - Technologies used (array),
+ * - Date created, and/or date last updated
+ * - Tags/Categories
+ * - License
+ * - Status (ongoing, completed, or in a specific development phase)
+ * - Download link
+ */
+
+export default defineType({
+  title: 'Projects',
   name: 'project',
-  title: 'Project',
   type: 'document',
   fields: [
-    {
-      name: 'projectTitle',
-      title: 'Project Title',
-      type: 'string'
-    },
-    {
-      name: 'links',
+    // Title
+    defineField({
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+      description: 'Project title.'
+    }),
+    // Image
+    defineField({
+      title: 'Thumbnail',
+      name: 'thumbnail',
+      type: 'image',
+      description: 'Image for the project.'
+    }),
+    // Links
+    defineField({
       title: 'Links',
-      type: 'string'
-    }
+      name: 'link',
+      type: 'object',
+      description: 'Github and Project links.',
+      fields: [
+        // Github
+        defineField({
+          title: 'Github',
+          name: 'github',
+          type: 'url',
+          description: 'Github link to the project.'
+        }),
+        // Project
+        defineField({
+          title: 'Project',
+          name: 'project_url',
+          type: 'url',
+          description: 'Any other link relevant to the project.'
+        }),
+        // Documentation
+        defineField({
+          title: 'Documentation',
+          name: 'documentation',
+          type: 'url',
+          description: 'Documentation link.'
+        })
+      ]
+    }),
+    // Featured
+    defineField({
+      title: 'Featured',
+      name: 'featured',
+      type: 'boolean',
+      description: 'Indicates whether this project is shown on the Featured section.'
+    })
   ]
-}
-
-type Links = {
-  github: string
-  project?: string
-}
+})
