c94fe04410fa64f4f756583d7d0b0cdc263aadd0
Red Williams <redpwilliams@gmail.com>
2023-11-30 05:33:59 -0500

    refactor(lint-staged): test add files that were linted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
-import "dotenv/config"
-import { defineConfig } from "astro/config"
-import { sanityIntegration as sanity } from "@sanity/astro"
-import react from "@astrojs/react"
+import 'dotenv/config'
+import { defineConfig } from 'astro/config'
+import { sanityIntegration as sanity } from '@sanity/astro'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: 'hybrid',
   integrations: [
     sanity({
+      // eslint-disable-next-line no-undef
       projectId: process.env.SANITY_PROJECT_ID,
-      dataset: "production",
-      apiVersion: "2021-03-25",
+      dataset: 'production',
+      apiVersion: '2021-03-25',
       useCdn: true,
-      studioBasePath: "/studio"
+      studioBasePath: '/studio'
     }),
     react()
   ],
