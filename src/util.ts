445b9cb469f5b8df6f966da18ab333abd10ab2a6
Red Williams <redpwilliams@gmail.com>
2023-12-23 02:44:50 -0500

    refactor: remove unnecessary type definitions for util functions

--- src/util.ts
+++ src/util.ts
@@ -6,16 +6,12 @@ import type { SanityResponse } from '@types'
 /**
  * Creates and returns the linear gradient attribute for a <defs> definition tag.
  *
- * @param {HTMLElement} icon - The HTML element to which the linear gradient
+ * @param icon - The HTML element to which the linear gradient
  * will be applied.
- * @param {number} index - The index of the linear gradient.
- * @param {string} id - The ID to be assigned to the linear gradient.
+ * @param index - The index of the linear gradient.
+ * @param id - The ID to be assigned to the linear gradient.
  */
-export function createLinearGradient(
-  icon: HTMLElement,
-  index: number,
-  id: string
-): SVGLinearGradientElement {
+export function createLinearGradient(icon: HTMLElement, index: number, id: string) {
   const linearGradient = document.createElementNS('http://www.w3.org/2000/svg', 'linearGradient')
   linearGradient.setAttribute('id', id)
   linearGradient.setAttribute('x1', '0%')
@@ -39,18 +35,15 @@ export function createLinearGradient(
 
 /**
  * Fetches data from Sanity studio.
- * @param {string} query - The sanity query to run.
- * @returns {Promise<{ data: SanityResponse, empty: boolean }>} A Promise that
- * resolves to an object containing the fetched data and an indicator of
- * whether the result is empty.
+ * @param query - The sanity query to run.
+ * @returns A Promise that resolves to an object containing
+ * the fetched data and an indicator of whether the result is empty.
  */
-export async function fetchSanityData(
-  query: string
-): Promise<{ data: SanityResponse; empty: boolean }> {
+export async function fetchSanityData(query: string) {
   let data: SanityResponse
   let empty: boolean
 
-  const PROJECT_ID = import.meta.env.PUBLIC_SANITY_PROJECT_ID
+  const PROJECT_ID = import.meta.env.PUBLIC_SANITY_PROJECT_ID as string
   const DATASET = 'production'
   const URL = `https://${PROJECT_ID}.api.sanity.io/v2022-03-07/data/query/${DATASET}?query=${query}`
 
