1aff141e9b1e5f7343bc4e4871e776217f6cc1e9
Red Williams <redpwilliams@gmail.com>
2024-02-08 23:56:26 -0500

    chore: add sanity docs reference link

--- schemas/RichTextComponents.tsx
+++ schemas/RichTextComponents.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// https://www.sanity.io/docs/customizing-the-portable-text-editor#14d3f8b767ae
 
 const H1 = (props: { children: React.ReactElement }) => {
   return <span style={{ fontSize: '1.75em', fontWeight: 600 }}>{props.children}</span>
