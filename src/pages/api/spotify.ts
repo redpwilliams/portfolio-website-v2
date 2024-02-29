ea506514885c924fb36adbbedcdea99cbaa4ef28
Red Williams <redpwilliams@gmail.com>
2024-02-29 16:48:59 -0500

    refactor: use refresh token in requests

--- src/pages/api/spotify.ts
+++ src/pages/api/spotify.ts
@@ -1,7 +1,26 @@
-export async function GET() {
-  const clientId = process.env.SPOTIFY_CLIENT_ID
-  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
+// Refresh token, defined on ther server
+const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN
+const clientId = process.env.SPOTIFY_CLIENT_ID
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
+let accessToken: string | null
 
+export async function GET() {
+  // Request access token if null
+  accessToken = accessToken ?? (await fetchAccessToken()).access_token
+  // const data = await fetchAccessToken()
+  // console.log(data)
+  console.log(accessToken)
+  // const data = await fetchAccessToken()
+  // console.log(data.json())
+  return new Response(JSON.stringify({ access_token: accessToken ?? '' }))
+}
+
+// const fetchSpotifyData = async () => {
+
+// }
+
+// Using refresh token
+const fetchAccessToken = async () => {
   const authOptions = {
     method: 'POST',
     headers: {
@@ -9,8 +28,9 @@ export async function GET() {
       'Content-Type': 'application/x-www-form-urlencoded' // Set content type to application/x-www-form-urlencoded
     },
     body: new URLSearchParams({
-      grant_type: 'client_credentials',
-      redirect_uri: 'http://localhost:3000/callback'
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken!,
+      client_id: clientId!
     })
   }
 
@@ -18,24 +38,29 @@ export async function GET() {
   const f = fetch('https://accounts.spotify.com/api/token', authOptions)
     .then((response) => {
       if (!response.ok) {
+        console.log(response)
         throw new Error('Network response was not ok')
       }
       return response.json()
     })
     .then((data) => {
-      const token = data.access_token // Access the access token from the response body
-      const token_type = data.token_type // Access the token type from the response body
-      const expires_in = data.expires_in // Access the expiration time from the response body
+      // const token = data.access_token // Access the access token from the response body
+      // const token_type = data.token_type // Access the token type from the response body
+      // const expires_in = data.expires_in // Access the expiration time from the response body
+
+      accessToken = data.acces_token
 
       // Use the obtained token for subsequent calls to Spotify Web API services
-      console.log('Access Token:', token)
-      console.log('Token Type:', token_type)
-      console.log('Expires In:', expires_in, 'seconds')
-      return new Response(JSON.stringify(data))
+      // console.log('Access Token:', token)
+      // console.log('Token Type:', token_type)
+      // console.log('Expires In:', expires_in, 'seconds')
+      return data
+      //return new Response(JSON.stringify(data))
     })
     .catch((error) => {
       console.error('There was a problem with the fetch operation:', error)
-      return new Response()
+      return ''
+      //return new Response()
     })
   return f
 }
