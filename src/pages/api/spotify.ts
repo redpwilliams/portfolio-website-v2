5798b7fec4d7e1264ddebcdd95585eb341ad7b9a
Red Williams <redpwilliams@gmail.com>
2024-02-29 20:49:06 -0500

    refactor: display spotify info in api route

--- src/pages/api/spotify.ts
+++ src/pages/api/spotify.ts
@@ -6,14 +6,19 @@ let accessToken: string | undefined
 
 export async function GET() {
   // Request access token if null
-  console.log(await fetchAccessToken())
   accessToken = accessToken ?? (await fetchAccessToken()).access_token
-  // const data = await fetchAccessToken()
-  // console.log(data)
-  // console.log(accessToken)
-  console.log(await fetchSpotifyData(accessToken!))
-  // const data = await console.log(data)
-  return new Response(JSON.stringify({ access_token: accessToken ?? '' }))
+
+  const data = await fetchSpotifyData(accessToken!)
+  const headers = new Headers()
+  headers.append('Content-Type', 'application/json')
+  // return new Response(buffer.toString('binary'), {
+  //   headers: {
+  //     'Content-Type': 'application/pdf'
+  //   }
+  // })
+  return new Response(JSON.stringify({ access_token: accessToken ?? '', data }), {
+    headers: { 'Content-Type': 'application/json' }
+  })
 }
 
 /**
@@ -21,7 +26,7 @@ export async function GET() {
  * @param accessToken - The access token required to access the Spotify Web API.
  * @returns A Response object to be sent to the client
  */
-const fetchSpotifyData = async (accessToken: string): Promise<Response> => {
+const fetchSpotifyData = async (accessToken: string) => {
   try {
     const res = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
       headers: { Authorization: 'Bearer ' + accessToken }
@@ -30,66 +35,52 @@ const fetchSpotifyData = async (accessToken: string): Promise<Response> => {
     // TODO - See what response I get when I try to do a 404,
     // I'd like to know how to format the error response for best practice and clarity
     if (!res.ok)
-      return new Response(
-        JSON.stringify({
-          data: null,
-          message: 'There was an error requesting data from Spotify. Status code ' + res.status
-        })
-      )
+      return {
+        data: null,
+        message: 'Unexpected error requesting data from Spotify, status code ' + res.status
+      }
 
     // I should only get 200 and 204:
     // https://developer.spotify.com/documentation/web-api/concepts/api-calls
     if (res.status == 204) {
-      return new Response(
-        JSON.stringify({
-          data: null
-        })
-      )
+      return {
+        data: {}
+      }
     }
 
     if (res.status == 200) {
       const data = await res.json()
-      return new Response(
-        JSON.stringify({
-          data: {
-            // Object of track's album name and link
-            album: {
-              name: data.item.album.name,
-              href: `https://open.spotify.com/album/${data.item.album.id}`
-            },
-            // Array of artists for the track
-            artists: [
-              data.item.artists.map((artist: { name: string; href: string }) => {
-                return {
-                  name: artist.name,
-                  href: artist.href
-                }
-              })
-            ],
-            // Track name and link
-            track: {
-              name: data.item.name,
-              href: `https://open.spotify/track/${data.item.id}`
-            }
+      return {
+        // Object of track's album name and link
+        album: {
+          name: data.item.album.name,
+          href: `https://open.spotify.com/album/${data.item.album.id}`
+        },
+        // Array of artists for the track
+        artists: data.item.artists.map((artist: { name: string; href: string }) => {
+          return {
+            name: artist.name,
+            href: `https://open.spotify.com/artist/${artist.id}`
           }
-        })
-      )
+        }),
+        // Track name and link
+        track: {
+          name: data.item.name,
+          href: `https://open.spotify.com/track/${data.item.id}`
+        }
+      }
     }
     // ^^^ Only works when content is playing (status code 200 most likely, i didn't check)
     // Else, it returns an empty body with status code 204
-    return new Response(
-      JSON.stringify({ data: null, message: 'Unexpected error with status code ' + res.status })
-    )
+    return { data: null, message: 'Unexpected error with status code ' + res.status }
   } catch (e: unknown) {
-    if (!(e instanceof SyntaxError)) return new Response(JSON.stringify({ data: null, error: e }))
-    return new Response(
-      JSON.stringify({ data: null, message: 'Most likely used .json() on a poor object' })
-    )
+    if (!(e instanceof SyntaxError)) return { data: null, error: e }
+    return { data: null, message: 'Most likely used .json() on a poor object' }
   }
 }
 
 // Using refresh token
-const fetchAccessToken = async () => {
+const fetchAccessToken = async (): Promise<{ access_token: string }> => {
   const authOptions = {
     method: 'POST',
     headers: {
