92d814d5229371137056c8fc3b0255e70a0e4c6c
Red Williams <redpwilliams@gmail.com>
2024-03-14 12:06:06 -0400

    feat: uses vercel kv

---
+++ src/pages/api/example.ts
@@ -0,0 +1,7 @@
+import { kv } from '@vercel/kv'
+import type { APIRoute } from 'astro'
+
+export const GET: APIRoute = async () => {
+  const data = await kv.hgetall('spotify')
+  return new Response(JSON.stringify(data))
+}
