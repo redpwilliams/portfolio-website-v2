c0a380f1d246136d486391ae9185c8fdff6c0d33
Red Williams <redpwilliams@gmail.com>
2024-04-04 13:21:38 -0400

    chore: add site url to config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://redwilliams.dev',
   output: 'server',
   adapter: vercel(),
   integrations: [
