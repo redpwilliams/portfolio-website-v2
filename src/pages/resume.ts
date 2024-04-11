6974cfeeed9fab376990763d20c3714f78207a7e
Red Williams <redpwilliams@gmail.com>
2024-04-11 13:22:32 -0400

    feat: include resume on /resume

---
+++ src/pages/resume.ts
@@ -0,0 +1,10 @@
+import type { APIRoute } from 'astro'
+import { readFileSync } from 'fs'
+import path from 'path'
+
+export const GET: APIRoute = async function get() {
+  const pdf = readFileSync(path.resolve('./public/resume.pdf'))
+  return new Response(pdf, {
+    headers: { 'Content-Type': 'application/pdf' }
+  })
+}
