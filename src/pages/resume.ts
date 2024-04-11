1f078d127845b672ebdeea52adc9180d4432fbe9
Red Williams <redpwilliams@gmail.com>
2024-04-11 19:01:14 -0400

    chore: remove ugly and unnecessary function name

--- src/pages/resume.ts
+++ src/pages/resume.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from 'astro'
 import { readFileSync } from 'fs'
 import path from 'path'
 
-export const GET: APIRoute = async function get() {
+export const GET: APIRoute = async () => {
   const pdf = readFileSync(path.resolve('./public/resume.pdf'))
   return new Response(pdf, {
     headers: { 'Content-Type': 'application/pdf' }
